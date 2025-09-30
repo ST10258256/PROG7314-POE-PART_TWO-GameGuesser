5f8184f62baf6060e2569807afb22f43d31c76ad
ST10258256 <130571591+ST10258256@users.noreply.github.com>
2025-09-30 18:12:32 +0200

    Allow the user to make the backend start automatically

---
+++ android-app/start-backend.bat
@@ -0,0 +1,10 @@
+@echo off
+REM =============================
+REM Start Backend API Script
+REM =============================
+
+REM Navigate to backend folder
+cd /d "C:\Users\luke jordan\Documents\Varsity College\2025\PROG7314 POE\PROG7314-POE-PART_TWO-GameGuesser\api\GameGuesserAPI"
+
+REM Run the API in a new command window
+start cmd /k "dotnet run"
